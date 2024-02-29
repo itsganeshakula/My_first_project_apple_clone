b02f92af8f5b196ddf3c4667cca751a46d810675
itsganeshakula <ganeshakulag@gmail.com>
2024-02-29 22:01:29 +0530

    innitial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+var x=document.getElementById("hover-parent");
+
+function visible(){
+    
+    x.style.display=("block");
+    x.style.transitionProperty=("height");
+    x.style.transitionDuration=("1s")
+    
+    
+}
+function hide(){
+    
+    x.style.display=("none");
+}
+
+
